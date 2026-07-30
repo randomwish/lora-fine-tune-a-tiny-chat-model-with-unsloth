5a39d8b3ac71a59868df329f6bc59eaab22f1405
Yenzhe Wee <hanzhe100100@gmail.com>
2026-07-31 00:47:45 +0800

    Step 14: count_tokens

--- model.py
+++ model.py
@@ -123,8 +123,11 @@ def tokenize_text(tokenizer, text):
     ids = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(text))
     return ids
 
-# Step 14 - count_tokens (not yet solved)
-# TODO: implement
+# Step 14 - count_tokens
+def count_tokens(input_ids):
+    """Return the number of tokens in a tokenized example."""
+    # TODO: return the length of the input_ids sequence
+    return len(input_ids)
 
 # Step 15 - build_training_arguments (not yet solved)
 # TODO: implement
