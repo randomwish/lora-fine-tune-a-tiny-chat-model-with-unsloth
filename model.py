cc7419ea54acb0cf9e346a18abe3a1b766b9b193
Yenzhe Wee <hanzhe100100@gmail.com>
2026-07-30 18:35:48 +0800

    Step 9: build_instruction_examples

--- model.py
+++ model.py
@@ -83,8 +83,16 @@ def trainable_fraction(trainable_count, total_count):
     # TODO: return the fraction of parameters that are trainable.
     return trainable_count / total_count
 
-# Step 9 - build_instruction_examples (not yet solved)
-# TODO: implement
+# Step 9 - build_instruction_examples
+def build_instruction_examples():
+    """Return a small list of {'instruction', 'response'} dicts for SFT."""
+    # TODO: return a tiny hand-written list of instruction/response example dicts.
+    return [
+        {"instruction": 'what is good', "response": 'prata'},
+        {"instruction": 'what is bad', "response": 'water'},
+        {"instruction": 'what is alright', "response": 'hmm... matcha maybe?'},
+
+    ]
 
 # Step 10 - format_instruction_example (not yet solved)
 # TODO: implement
